146ef7f0df7f32c8ccf033a37e7255e1320c6f4a
bmangeard <mangeard.benoit@gmail.com>
2018-01-05 13:15:49 +0100

    Frist look Website

---
+++ index.php
@@ -0,0 +1,259 @@
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Charlotte Malétras</title>
+	<meta name="viewport" content="width=device-width, height=device-height, user-scalable=no, maximum-scale = 1.0, minimum-scale=1.0,initial-scale=1.0" >
+	<link rel="stylesheet" href="font_awesome_5.0.2/web-fonts-with-css/css/fontawesome-all.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="menu">
+		<div class="container mb-5" id="cont_menu">
+			<div class="main">
+				<div class="row mt-3 ml-1">
+					<div class="col text-left">
+						<div class="logo">
+							<span class="icon"><img src="img/logo.png" alt="logo"></span>
+						</div>
+					</div>
+				</div>	
+				<div class="row mt-3">
+					<div class="col text-center">
+						<div class="content">
+							<br>
+							<h2 class="text-white">Montage vidéo | Le Havre</h2>
+							<p class="text-white">Freelance dans le montage vidéo, je vous accompagne pour la succès de vos visuels, que vous soyez un professionnel ou un particulier.</p>
+							<br>
+						</div>
+					</div>
+				</div>	
+				<div class="row mb-3">
+					<div class="col text-center">
+						<div class="btn-group" role="group" aria-label="Basic example">
+							<button id="btn_propos" type="button" class="btn btn-outline-light" onclick="propos()">A propos</button>
+							<button id="btn_real" type="button" class="btn btn-outline-light" onclick="real()">Réalisations</button>
+							<button id="btn_contact" type="button" class="btn btn-outline-light" onclick="contact()">Contact</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container mb-5" id="cont_propos" style="display: none">
+			<div class="main">
+				<div class="row mt-3 mr-1">
+					<div class="col text-right">
+							<button id="btn_close" type="button" class="btn btn-outline-light" onclick="propos()">X</button>
+					</div>
+				</div>				
+				<div class="row mt-3 ml-4">
+					<div class="col text-left">
+						<div class="content">
+							<h2 class="text-white"><u>A propos :</u></h2>
+						</div>
+					</div>
+				</div>	
+				<div class="row mt-3 ml-4 mb-4 mr-4">
+					<div class="col-12 text-center">
+						<div class="content">
+							<img src="img/photo.jpg" alt="photo" id="photo"/>
+						</div>
+					</div>
+					<div class="col-12 text-left mt-4">
+						<div class="content">
+							<p class="text-white">
+								De formation DUT Métiers du multimédia et de l'Internet (anciennement Services et Réseaux de Communications) à l'antenne d'Elbeuf de l'université de Rouen, j'ai par la suite travaillé plusieurs années à Paris pour une chaîne de télévision. J'y ai appris le montage, la réalisation, le cadrage et divers métiers de l'audiovisuel, j'ai décidé de me spécialisé dans le montage vidéo.
+							<br>
+							<br>
+								Passionné de vidéo, j'ai enrichit mes compétences en montage vidéo avec différents clients, tel que : la chaîne de télévision STAR 24, le groupe de radio 1981 (ADO, VOLTAGE), Sud Radio, montage son, blogueuses.
+							<br>
+								En plus du montage vidéo, je fais du doublage et de la voix off pour STAR24.
+							<br>
+							<br>	
+								Aujourd'hui, monteuse vidéo à mon compte au Havre, je souhaite vous proposer mes services et vous accompagner dans votre créativité.
+							<br>
+							<br>
+								Logiciels : Suite Adobe, Premier PRO, Photoshop, After Effects, Illustrator, InDesign...
+							<br>
+							<br>
+								Matériel : Dell XPS, Digidesign MBOX 2 PRO, Monitoring Prodipe Pro 8, Microphone AKG Peception 420 + Filtre anti-pop, Casque Sennheiser HD25, Go Pro Hero 3, Canon 1100D
+							</p>
+						</div>
+					</div>
+				</div>	
+			</div>
+		</div>
+		<div class="container mb-5" id="cont_real" style="display: none">
+			<div class="main">
+				<div class="row mt-3 mr-1">
+					<div class="col text-right">
+							<button id="btn_close" type="button" class="btn btn-outline-light" onclick="real()">X</button>
+					</div>
+				</div>	
+				<div class="row mt-3 ml-4">
+					<div class="col text-left">
+						<h3 class="text-white"><u>Réalisations :</u></h3>
+						<p class="text-white">Mon activitée est très variées, télévisions, radios, vidéos youtube, mariages, compétitions sportives...<br>
+						En voici quelques exemples :</p>
+					</div>
+				</div>		
+				<div class="row mt-3">
+					<div class="col text-center">
+  						<img src="img/star24.jpg" alt="star24" id="logo_clients"/>
+					</div>
+					<div class="col text-center">
+  						<img src="img/star24.jpg" alt="star24" id="logo_clients"/>
+					</div>
+					<div class="col text-center">
+  						<img src="img/star24.jpg" alt="star24" id="logo_clients"/>
+					</div>
+				</div>
+				<div class="row mt-3 mb-4">
+					<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 text-center">
+						<div class="content">
+							<p class="text-white">Star 24 (Montage et voix off)</p>
+							<object type="application/x-shockwave-flash" width="425" height="355" data="https://www.youtube.com/v/5UyzJrqhUZg">
+								<param name="movie" value="https://www.youtube.com/v/5UyzJrqhUZg" />
+								<param name="wmode" value="transparent" />
+							</object>
+							<!--[if lte IE 6 ]>
+								<embed src="https://www.youtube.com/watch?v=5UyzJrqhUZg" type="application/x-shockwave-flash" wmode="transparent" width="425" height="355"></embed>
+							<![endif]-->
+						</div>	
+					</div>	
+					<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 text-center">
+						<p class="text-white">LHUT (Montage)</p>
+						<object type="application/x-shockwave-flash" width="425" height="355" data="https://www.youtube.com/v/wL-88DpVC70">
+							<param name="movie" value="https://www.youtube.com/v/wL-88DpVC70" />
+							<param name="wmode" value="transparent" />
+						</object>
+						<!--[if lte IE 6 ]>
+							<embed src="https://www.youtube.com/v/wL-88DpVC70" type="application/x-shockwave-flash" wmode="transparent" width="425" height="355"></embed>
+						<![endif]-->
+					</div>
+				</div>		
+			</div>
+		</div>
+		<div class="container mb-5" id="cont_contact" style="display: none">
+			<div class="main">
+				<div class="row mt-3 mr-1">
+					<div class="col text-right">
+							<button id="btn_close" type="button" class="btn btn-outline-light" onclick="contact()">X</button>
+					</div>
+				</div>				
+				<div class="row ml-4 mr-4">
+					<div class="col">
+						<div class="text-white text-left">
+							<h2><u>Contact :</u></h2>
+							<p>N'hésitez pas à me contacter pour tout devis ou bien pour toutes questions concernant vos projets.</p>
+						</div>
+					</div>
+				</div>	
+				<div class="row mt-3 mb-3 ml-2 mr-2">
+					<div class="col text-center">
+						<div class="content">
+							<div class="container text-white">
+								<div class="row">
+									<div class="col">
+										<form class="form-horizontal" method="post" action="formulaire.inc.php">
+											<div class="form-group">
+												<label for="InputPrenom">Prénom</label>
+												<input type="text" class="form-control" id="InputPrenom" name="InputPrenom" placeholder="Votre prénom" required>
+											</div>
+											<div class="form-group">
+												<label for="InputName">Nom</label>
+												<input type="text" class="form-control" id="InputName" name="InputName" placeholder="Votre nom" required>
+											</div>
+											<div class="form-group">
+												<label for="InputEmail">Email</label>
+												<input type="email" class="form-control" id="InputEmail" name="InputEmail" placeholder="nom@exemple.com" required>
+											</div>
+											<div class="form-group ">
+												<label for="InputText">Votre message</label>
+												<textarea  class="form-control" id="InputText" name="InputText" placeholder="Votre message" required></textarea> 
+											</div>
+											<div class="form-check text-left">
+												<input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+												<label class="form-check-label" for="exampleRadios1">
+												Vous êtes un professionnel.
+												</label>
+											</div>
+											<div class="form-check text-left">
+												<input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+												<label class="form-check-label" for="exampleRadios2">
+												Vous êtes un particulier.
+												</label>
+											</div>
+											<button type="submit" class="btn btn-outline-light">Envoyer</button>
+										</form>
+									</div>
+								</div>	
+								<div class="row mt-4" id="contact_sep">
+									<div class="col">
+										<ul class="list-inline text-left">
+											<br>
+											<li><label><i class="fa fa-phone" aria-hidden="true"></i> | 06 69 47 40 98</label>
+											<li><label><i class="fa fa-envelope" aria-hidden="true"></i>  | contact@charlottemaletras.com</label>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>	
+			</div>
+		</div>
+	</div>
+	
+	<footer>
+		<div class="text-center text-white fixed-bottom">
+			<br>
+			<p id="copyright">&copy; 2018 - CHARLOTTE MALETRAS | DESIGN: BENOIT MANGEARD</p>
+		</div>
+	</footer>
+	<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="jQuery/jquery-3.2.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+	<script>
+		function propos() {
+			var v_menu = document.getElementById("cont_menu");
+			var v_propos = document.getElementById("cont_propos");
+			if (v_menu.style.display === "none") {
+				v_menu.style.display = "block";
+				v_propos.style.display = "none";
+			} else {
+				v_menu.style.display = "none";
+				v_propos.style.display = "block";
+			}
+		}
+		function real() {
+			var v_menu = document.getElementById("cont_menu");
+			var v_real = document.getElementById("cont_real");
+			if (v_menu.style.display === "none") {
+				v_menu.style.display = "block";
+				v_real.style.display = "none";
+			} else {
+				v_menu.style.display = "none";
+				v_real.style.display = "block";
+			}
+		}
+		function contact() {
+			var v_menu = document.getElementById("cont_menu");
+			var v_contact = document.getElementById("cont_contact");
+			if (v_menu.style.display === "none") {
+				v_menu.style.display = "block";
+				v_contact.style.display = "none";
+			} else {
+				v_menu.style.display = "none";
+				v_contact.style.display = "block";
+			}
+		}
+	</script>
+	
+</body>
+</html>
